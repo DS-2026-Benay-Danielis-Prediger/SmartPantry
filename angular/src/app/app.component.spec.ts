3db7c34b9c39d772ef821dbf1c974b04fe079be2
francobenay <francobenay23@gmail.com>
2026-09-16 18:27:19 -0300

    Create app.component.spec.ts

---
+++ angular/src/app/app.component.spec.ts
@@ -0,0 +1,17 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
+  });
+
+  it('debería crear el componente principal', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+
+    expect(component).toBeTruthy();
+  });
+});
