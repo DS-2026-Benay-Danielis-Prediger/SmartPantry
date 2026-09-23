b78af6524e44f82ee1bd7d34b1a0ffc2c0ce030c
francobenay <francobenay23@gmail.com>
2026-09-23 11:30:05 -0300

    Create producto.cs

---
+++ src/SmartPantry.Domain/Productos/producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities;
+
+namespace SmartPantry.Productos;
+
+public class Producto : BasicAggregateRoot<Guid>
+{
+    public string CodigoBarras { get; set; } = string.Empty;
+    public string Nombre { get; set; } = string.Empty;
+    public string Marca { get; set; } = string.Empty;
+    public List<string> Ingredientes { get; set; } = new();
+    public List<string> Alergenos { get; set; } = new();
+}
